86a9a2e31f68627f5a415104f9a301f5b78a5f52
my name <blueshaperd3@gmail.com>
2024-09-03 02:24:39 +0500

    renderTable and submit ok

--- app.js
+++ app.js
@@ -9,3 +9,39 @@ let phone = document.getElementById("phone");
 let tableBody = document.getElementById("tableBody");
 let searchInp = document.getElementById("searchInp");
 let searchKey = document.getElementById("searchKey");
+
+
+function renderTable(){
+    tableBody.innerHTML = "";
+    for(var i = 0; i < arr.length; i++){
+        var x = arr[i];
+    tableBody.innerHTML += `
+    <tr>
+    <td>${x.firstName}</td>
+    <td>${x.lastName}</td>
+    <td>${x.age}</td>
+    <td>${x.cnic}</td>
+    <td>${x.phone}</td>
+    </tr>
+    `; 
+}
+}
+
+function btnClick() {
+model.firstName = firstName.value;
+model.lastName = lastName.value;
+model.age = age.value;
+model.cnic = cnic.value;
+model.phone = phone.value;
+
+arr.push({...model});
+
+firstName.value= "";
+lastName.value= "";
+age.value= "";
+cnic.value= "";
+phone.value= "";
+
+console.log(arr);
+renderTable()
+}
