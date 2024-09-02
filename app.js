bb1252f2209dea672f589c068be2404ea1ab6fad
my name <blueshaperd3@gmail.com>
2024-09-03 01:40:33 +0500

    get elements done

---
+++ app.js
@@ -0,0 +1,11 @@
+let arr = [];
+let model = {};
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let age = document.getElementById("age");
+let cnic = document.getElementById("cnic");
+let phone = document.getElementById("phone");
+let tableBody = document.getElementById("tableBody");
+let searchInp = document.getElementById("searchInp");
+let searchKey = document.getElementById("searchKey");
